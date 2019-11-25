66ba1289c1df5f696b384ccc8a472f2003a3c2c4
ksk02 <nahanaha02@gmail.com>
2019-11-26 03:21:38 +0900

    電波強度 実装中

--- arkit_traffic/arkit_traffic/ViewController.swift
+++ arkit_traffic/arkit_traffic/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 import SceneKit
 import ARKit
+import CoreBluetooth
+import NetworkExtension
 
 class ViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet var sceneView: ARSCNView!
@@ -49,16 +51,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
     }
 
-    @IBAction func calcTrafficStrength(_ sender: Any) {
-        var trafficStrength = 0;
-        if (getWifiNumberOfActiveBars() != nil) {
-            trafficStrength = getWifiNumberOfActiveBars() ?? 0
-        }
-
-        if trafficStrength <= 0 {
-            self.sphereColor = UIColor.blue
-        }
-    }
+//    @IBAction func calcTrafficStrength(_ sender: Any) {
+//        var trafficStrength = 0;
+//        if (getWifiNumberOfActiveBars() != nil) {
+//            trafficStrength = getWifiNumberOfActiveBars() ?? 0
+//        }
+//
+//        if trafficStrength <= 0 {
+//            self.sphereColor = UIColor.blue
+//        }
+//    }
 
 
     override func viewDidLoad() {
@@ -89,6 +91,22 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         guard let touch = touches.first else {return}
 
         let touchPos = touch.location(in: sceneView)
+        print("こんにちは")
+        var peripheral: CBPeripheral?
+        peripheral?.readRSSI()
+        if (peripheral != nil) {
+            peripheral?.readRSSI()
+            print(peripheral?.readRSSI())
+        }
+        print(peripheral ?? 777)
+        print("あああ")
+        
+//        var rssiTest: Int
+//        rssiTest = getSignalStrength()
+//        print(rssiTest)
+        
+//        getWifiNumberOfActiveBars()
+        printRetrievedWifiNetwork()
 
         // タップされた位置のARアンカーを探す
         let hitTest = sceneView.hitTest(touchPos, types: .existingPlaneUsingExtent)
@@ -119,29 +137,90 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         guard !(anchor is ARPlaneAnchor) else { return }
     }
+    
+    // https://codeday.me/jp/qa/20190714/1258248.html
+//    func peripheral(peripheral: CBPeripheral, didReadRSSI RSSI: NSNumber, error: NSError?) {
+//        print("Peripheral!!!!!!!!!")
+//        print("RSSI = \(RSSI)")
+//    }
+    
+    // https://stackoverflow.com/questions/54039211/how-to-get-signal-strength-rsrp-rssi-rsrq-sinr-cell-id-lte-3g-2g-of-iphone
+    func getSignalStrength() -> Int {
+
+        let application = UIApplication.shared
+        let statusBarView = application.value(forKey: "statusBar") as! UIView
+        let foregroundView = statusBarView.value(forKey: "foregroundView") as! UIView
+        let foregroundViewSubviews = foregroundView.subviews
+
+        var dataNetworkItemView:UIView!
+
+        for subview in foregroundViewSubviews {
+            if subview.isKind(of: NSClassFromString("UIStatusBarSignalStrengthItemView")!) {
+                dataNetworkItemView = subview
+                break
+            } else {
+                return 0 //NO SERVICE
+            }
+        }
+
+        return dataNetworkItemView.value(forKey: "signalStrengthBars") as! Int
+
+    }
 
     private func getWifiNumberOfActiveBars() -> Int? {
         let app = UIApplication.shared
         var numberOfActiveBars: Int?
-        guard let containerBar = app.value(forKey: "statusBar") as? UIView else { return nil }
+        // 自分で追加
+        var statusBarManager: UIStatusBarManager?
+//        guard let containerBar = app.value(forKey: "statusBar") as? UIView else { return nil }
+        // 自分で追加
+        guard let containerBar = UIApplication.shared.keyWindow as? UIView else { return nil }
+        print(containerBar)
+        print("BBB")
+        return 0
+        // 自分で追加
+//        statusBarManager = UIApplication.shared.keyWindow?.windowScene?.statusBarManager
         guard let statusBarMorden = NSClassFromString("UIStatusBar_Modern"), containerBar .isKind(of: statusBarMorden), let statusBar = containerBar.value(forKey: "statusBar") as? UIView else { return nil }
+        print(statusBarMorden)
+        print("ccc")
+//
+//        guard let foregroundView = statusBar.value(forKey: "foregroundView") as? UIView else { return nil }
+//
+//        for view in foregroundView.subviews {
+//            for v in view.subviews {
+//                if let statusBarWifiSignalView = NSClassFromString("_UIStatusBarWifiSignalView"), v .isKind(of: statusBarWifiSignalView) {
+//                    if let val = v.value(forKey: "numberOfActiveBars") as? Int {
+//                        numberOfActiveBars = val
+//                        break
+//                    }
+//                }
+//            }
+//            if let _ = numberOfActiveBars {
+//                break
+//            }
+//        }
 
-        guard let foregroundView = statusBar.value(forKey: "foregroundView") as? UIView else { return nil }
+//        return numberOfActiveBars
+    }
 
-        for view in foregroundView.subviews {
-            for v in view.subviews {
-                if let statusBarWifiSignalView = NSClassFromString("_UIStatusBarWifiSignalView"), v .isKind(of: statusBarWifiSignalView) {
-                    if let val = v.value(forKey: "numberOfActiveBars") as? Int {
-                        numberOfActiveBars = val
-                        break
-                    }
-                }
-            }
-            if let _ = numberOfActiveBars {
-                break
-            }
+    func printRetrievedWifiNetwork() {
+        let interfaces = NEHotspotHelper.supportedNetworkInterfaces()
+
+        print("--- \(interfaces)") // Appleの許可が得られるまで、常に空
+
+        for interface in interfaces as! [NEHotspotNetwork] {
+            print("--- \(interfaces)")
+            let ssid = interface.ssid
+            let bssid = interface.bssid
+            let secure = interface.isSecure
+            let autoJoined = interface.didAutoJoin
+            let signalStrength = interface.signalStrength
+
+            print("ssid: \(ssid)")
+            print("bssid: \(bssid)")
+            print("secure: \(secure)")
+            print("autoJoined: \(autoJoined)")
+            print("signalStrength: \(signalStrength)")
         }
-
-        return numberOfActiveBars
     }
 }
